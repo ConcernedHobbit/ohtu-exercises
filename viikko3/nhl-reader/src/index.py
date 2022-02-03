c0b3df1e2cb698c887a871da9ee8d17384c38876
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-03 17:12:58 +0200

    implement player fetching functionality

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,24 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    res = requests.get(url).json()
+    
+    players = []
+    for player_dict in res:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            int(player_dict['assists']),
+            int(player_dict['goals']),
+            int(player_dict['penalties']),
+            player_dict['team'],
+            int(player_dict['games'])
+        )
+        players.append(player)
+
+    print(*players, sep='\n')
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,21 @@
 class Player:
-    def __init__(self, name):
+    def __init__(
+        self, 
+        name: str, 
+        nationality: str, 
+        assists: int, 
+        goals: int, 
+        penalties: int, 
+        team: str, 
+        games: int
+    ):
         self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
     
     def __str__(self):
-        return self.name
+        return f"[{self.team}] [{self.nationality}] {self.name}: {self.goals} goals, {self.assists} assists"
