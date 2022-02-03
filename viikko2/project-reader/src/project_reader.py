4fad0ad0763df3ca481b4637fb58485cf725d7b5
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-03 09:55:59 +0200

    implement rudimentary toml parsing

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,12 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        parsed_toml = toml.loads(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(
+            parsed_toml['tool']['poetry']['name'], 
+            parsed_toml['tool']['poetry']['description'], 
+            parsed_toml['tool']['poetry']['dependencies'].keys(), 
+            parsed_toml['tool']['poetry']['dev-dependencies'].keys()
+        )
