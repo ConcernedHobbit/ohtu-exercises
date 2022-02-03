24f9add7571f61a8ff5c7c2eddd9b764856032a0
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-03 10:59:24 +0200

    blast away singletons

--- viikko2/verkkokauppa-1/src/varasto.py
+++ viikko2/verkkokauppa-1/src/varasto.py
@@ -3,17 +3,8 @@ from kirjanpito import Kirjanpito
 
 
 class Varasto:
-    __instanssi = None
-
-    @staticmethod
-    def get_instance():
-        if not Varasto.__instanssi:
-            Varasto.__instanssi = Varasto()
-
-        return Varasto.__instanssi
-
-    def __init__(self):
-        self._kirjanpito = Kirjanpito.get_instance()
+    def __init__(self, kirjanpito: Kirjanpito):
+        self._kirjanpito = kirjanpito
         self._saldot = {}
         self._alusta_tuotteet()
 
