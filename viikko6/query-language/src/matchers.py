9924c7bb70d7e47274c66e327de4b510de02ed84
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-05 01:59:02 +0200

    implement All, Not, HasFewerThan

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -25,3 +25,24 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+class All:
+    def matches(self, player):
+        return True
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def matches(self, player):
+        return not self._matcher.matches(player)
