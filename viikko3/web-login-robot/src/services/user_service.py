5a0ab70bea8fc9a65fb8364bc4a8c1e9a14644e2
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-04 18:02:44 +0200

    implement registration checks

--- viikko3/web-login-robot/src/services/user_service.py
+++ viikko3/web-login-robot/src/services/user_service.py
@@ -1,3 +1,4 @@
+import re
 from entities.user import User
 from repositories.user_repository import (
     user_repository as default_user_repository
@@ -40,7 +41,21 @@ class UserService:
         if not username or not password:
             raise UserInputError("Username and password are required")
 
-        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if not password_confirmation:
+            raise UserInputError("Password confirmation is required")
+
+        if password != password_confirmation:
+            raise UserInputError("Passwords don't match")
+
+        if len(username) < 3:
+            raise UserInputError("Username should be at least 3 characters")
+
+        if len(password) < 8:
+            raise UserInputError("Password should be at least 8 characters")
+
+        if re.match("^[a-z]+$", password):
+            raise UserInputError(
+                "Password should have non-alphanumeric characters")
 
 
 user_service = UserService()
