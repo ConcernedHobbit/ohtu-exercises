74824c2968d713037c8ca4409aa404203d615787
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-04 19:46:40 +0200

    implement tavaroita_korissa for initial state

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -1,16 +1,17 @@
 from tuote import Tuote
 from ostos import Ostos
 
+
 class Ostoskori:
     def __init__(self):
         pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        pass
+        return 0
         # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
 
     def hinta(self):
         return 0
