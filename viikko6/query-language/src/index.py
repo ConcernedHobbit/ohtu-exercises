503a08d2643d5b37008f426735854591e5b4afe4
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-05 03:00:00 +0200

    implement or building

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,7 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from query_builder import QueryBuilder
+from matchers import Or, And, PlaysIn
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
@@ -8,7 +9,23 @@ def main():
     stats = Statistics(reader)
 
     query = QueryBuilder()
-    matcher = query.playsIn("NYR").hasAtLeast(5, "goals").hasFewerThan(10, "goals").build()
+    
+    m1 = (
+        query
+            .playsIn("PHI")
+            .hasAtLeast(10, "assists")
+            .hasFewerThan(5, "goals")
+            .build()
+    )
+
+    m2 = (
+        query
+            .playsIn("EDM")
+            .hasAtLeast(40, "points")
+            .build()
+    )
+
+    matcher = query.oneOf(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,21 +1,56 @@
-import matchers
+from matchers import All, PlaysIn, HasAtLeast, HasFewerThan, Or, And, Not
 
 class QueryBuilder:
-  def __init__(self, matchers = [], matcher = matchers.All()):
+  def __init__(self, matchers = [], matcher = None, combiner = And):
+    self._combiner = combiner
     self._matchers = matchers
-    self._matchers.append(matcher)
+
+    if matcher:
+      self._matchers.append(matcher)
 
   def playsIn(self, team):
-    return QueryBuilder(self._matchers, matchers.PlaysIn(team))
+    return QueryBuilder(
+      self._matchers, 
+      PlaysIn(team),
+      self._combiner
+    )
 
   def hasAtLeast(self, value, attr):
-    return QueryBuilder(self._matchers, matchers.HasAtLeast(value, attr))
+    return QueryBuilder(
+      self._matchers, 
+      HasAtLeast(value, attr),
+      self._combiner
+    )
 
   def hasFewerThan(self, value, attr):
-    return QueryBuilder(self._matchers, matchers.HasFewerThan(value, attr))
+    return QueryBuilder(
+      self._matchers, 
+      HasFewerThan(value, attr),
+      self._combiner
+    )
 
   def all(self):
-    return QueryBuilder(self._matchers, matchers.All())
+    return QueryBuilder(
+      self._matchers, 
+      All(),
+      self._combiner
+    )
+
+  def oneOf(self, *matchers):
+    return QueryBuilder(
+      [*matchers],
+      None,
+      Or
+    )
 
   def build(self):
-    return matchers.And(*self._matchers)
+    matchers = self._matchers[:]
+    self._matchers.clear()
+
+    if len(matchers) == 0:
+      return All()
+
+    if len(matchers) == 1:
+      return matchers[0]
+
+    return self._combiner(*matchers)
